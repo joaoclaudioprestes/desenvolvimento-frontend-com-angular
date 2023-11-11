8f9867a9eebfd3223c9348462d0544b75db6aefa
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-11-11 14:49:51 -0300

    :up:

--- 5_objetos/aula01.js
+++ 5_objetos/aula01.js
@@ -1,8 +1,24 @@
 const joao = {
     nome: 'João Prestes',
-    idade: 18
+    idade: 18,
+    descrever: function () {
+        console.log(`
+        Meu nome é: ${this.nome}
+        Minha idade é: ${this.idade}
+        `);
+    }
 };
 
+console.log(typeof joao);
 console.log(joao);
 console.log(joao.nome);
-console.log(typeof joao);
+
+joao.altura = 1.75
+
+console.log(joao.altura)
+
+delete joao.altura
+
+console.log(joao.altura)
+
+joao.descrever()
