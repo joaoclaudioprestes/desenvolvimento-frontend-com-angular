b3caa1a38dcf4e68baccc5c0bcd6893c03f8a289
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2023-11-15 14:52:24 -0300

    :up:

--- 1_variaveis_e_operadores/aula.js
+++
@@ -1,7 +0,0 @@
-// Variaveis
-let variavel1 = 10;
-variavel1 = 20; //Pode ser redefinida
-const pi = 3.14; // Não podemos redefinir
-
-console.log(variavel1);
-console.log(pi);
--- 8_pratica_array_repeticao/1_tabuada.js
+++
@@ -1,6 +0,0 @@
-let number = 8
-
-for (let i = 0; i <= 10; i++) {
-    let resultado = number * i
-    console.log(`${number} x ${i} = ${resultado}`)
-}
